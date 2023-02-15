59403d5b01cbefcfc8403b3cba9d8051690f385a
Oberdan <oberdan.dev@gmail.com>
2023-02-15 18:04:14 -0400

    add listiner to buttons in js

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const renderResult = document.querySelector('#render-result');
-const buttonOne = document.querySelector('#one')
+const buttons = document.querySelectorAll('button')
+const lastCalc = document.querySelector('#last-calc');
 
-console.log(renderResult, buttonOne)
+let previousValue;
+let actualValue;
+let operation;
 
-buttonOne.addEventListener('click', () =>
-{
-    renderResult.textContent = '1'
+const operations = {
+    soma: () => {
+
+    }
+}
+
+const previousContent = {}
+
+console.log(renderResult, buttons)
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', ({target}) =>{      
+    if(button.dataset.number){
+      renderResult.textContent += target.dataset.number
+      return
+    } 
+
+    if(button.id == 'C'){
+      renderResult.textContent = '';
+      return
+    }
+
+    if(button.id == 'plus'){
+      previousValue = Number(renderResult.textContent)
+      operation = 'plus'
+      renderResult.textContent = ''
+      lastCalc.textContent = `${previousValue} + `
+      return
+    }
+
+    if(button.id == 'equals'){  
+        if(operation == 'plus'){
+            actualValue = Number(renderResult.textContent)
+            let newValue = previousValue + actualValue;
+            renderResult.textContent = newValue
+            lastCalc.textContent += `${actualValue}`
+            return;
+        }
+    }
+    })
+    
+
 })
-
